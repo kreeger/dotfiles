35f3b784ad46de2fd6fde38539c77d167684b0d9
Ben Kreeger <ben@kree.gr>
2023-04-17 21:20:15 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #/usr/bin/env bash
 
 # Setup script.
-git clone --recurse-submodules https://github.com/kreeger/zshrc.git $HOME/.zsh
-ln -sfv $HOME/.zsh/zshrc $HOME/.zshrc
+if [[ ! -d "$HOME/.zsh" ]]; then
+    git clone --recurse-submodules https://github.com/kreeger/zshrc.git $HOME/.zsh
+fi
+
+if [[ ! -a "$HOME/.zshrc" ]]; then
+    ln -sfv $HOME/.zsh/zshrc $HOME/.zshrc
+fi
+
+# Link dotfiles.
+declare -a dotfiles=("gemrc" "gitconfig" "gitignore.global")
+
+for file in "${dotfiles[@]}"
+do
+    if [[ ! -a "$HOME/.$file" ]]; then
+        ln -sfv $HOME/.dotfiles/.$file $HOME/.$file
+    fi
+done
